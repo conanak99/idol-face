1dea12417a53b166dfaf319e5c94ec1fcf92405b
Hoang Pham <huyhoang8a5@gmail.com>
2017-01-15 21:36:49 +0800

    Add VAV idol demo

---
+++ vav/script.js
@@ -0,0 +1,66 @@
+// Code goes here
+var app = angular.module('app', []);
+
+app.factory('recognizeService', function($http) {
+    return {
+        recognize: function(imgLink) {
+            var url = 'https://wt-huyhoang8a5-gmail-com-0.run.webtask.io/idol-recognize';
+            return $http({
+                method: 'POST',
+                url,
+                data: {
+                    url: imgLink
+                }
+            });
+        }
+    }
+});
+
+app.controller('mainCtrl', function($scope, recognizeService) {
+    $scope.isLoading = false;
+
+    $scope.$watch('imageLink', function(oldValue, newValue) {
+        $scope.faces = [];
+        $scope.faceDisplay = [];
+    });
+
+    $scope.recognize = function() {
+        if ($scope.isLoading)
+            return;
+
+        $scope.isLoading = true;
+        recognizeService.recognize($scope.imageLink).then(result => {
+            $scope.faces = result.data;
+
+            $scope.faceDisplay = result.data.map(rs => {
+                return {
+                    style: {
+                        top: rs.face.top + 'px',
+                        left: rs.face.left + 'px',
+                        width: rs.face.width + 'px',
+                        height: rs.face.height + 'px'
+                    },
+                    name: rs.idol.name
+                }
+            });
+            $scope.isLoading = false;
+        });
+    }
+
+    $scope.testImages = ["http://tse3.mm.bing.net/th?id=OIP.M62d737028ee51f22482fab76bdfe112do1&pid=15.1", "http://tse4.mm.bing.net/th?id=OIP.M93d1646690a0f345e561a80523529bb2o1&pid=15.1", "http://media.ngoisao.vn/resize_580/news/2014/11/30/miu-le-20.jpg", "http://static.giaoducthoidai.vn/uploaded/hainv/2016_01_27/images16422691452168028hotgirlhaiphongxinhnhumong191657_uzve.jpg?width=500"];
+
+    $scope.idols = [
+        "Ngọc Trinh",
+        "Bà tưng",
+        "Hường Hana",
+        "Hoàng Thùy Linh",
+        "Elly Trần",
+        "Thuỷ Top",
+        "Tâm Tít",
+        "Midu",
+        "Miu Lê",
+        "Chi Pu",
+        "Khả Ngân",
+        "Angela Phương Trinh"
+    ];
+});
