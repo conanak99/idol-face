e2a2d5ca69fa08eb11627c0843d5c4f2ab43d77f
Hoang Pham <huyhoang8a5@gmail.com>
2017-04-08 13:22:08 +0700

    Maintainance

--- vav/script.js
+++ vav/script.js
@@ -24,14 +24,17 @@ app.controller('mainCtrl', function($scope, recognizeService) {
         $scope.faceDisplay = [];
     });
 
+    // Gọi hàm này khi người dùng click button "Nhận diện"
     $scope.recognize = function() {
         if ($scope.isLoading)
             return;
 
         $scope.isLoading = true;
+        // Gọi hàm recognize của service
         recognizeService.recognize($scope.imageLink).then(result => {
             $scope.faces = result.data;
 
+            // Dựa vào kết quả trả về để set style động cho class idol-face
             $scope.faceDisplay = result.data.map(rs => {
                 return {
                     style: {
@@ -47,8 +50,10 @@ app.controller('mainCtrl', function($scope, recognizeService) {
         });
     }
 
+    // Danh sách ảnh để test
     $scope.testImages = ["http://tse3.mm.bing.net/th?id=OIP.M62d737028ee51f22482fab76bdfe112do1&pid=15.1", "http://tse4.mm.bing.net/th?id=OIP.M93d1646690a0f345e561a80523529bb2o1&pid=15.1", "http://media.ngoisao.vn/resize_580/news/2014/11/30/miu-le-20.jpg", "http://static.giaoducthoidai.vn/uploaded/hainv/2016_01_27/images16422691452168028hotgirlhaiphongxinhnhumong191657_uzve.jpg?width=500"];
 
+    // Danh sách idol
     $scope.idols = [
         "Ngọc Trinh",
         "Bà tưng",
